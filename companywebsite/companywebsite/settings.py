0bd220a88dd52770697de175bc6c452897bea554
dhivyapriyar <dhivyapriya2611@gmail.com>
2023-01-29 16:24:43 +0000

    Success

--- companywebsite/companywebsite/settings.py
+++ companywebsite/companywebsite/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-!^&0uvezr#*umls^r&q99r_bvh*%)*p9vgy&f7uvs=h@s-@uy_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'dhivyapriya.student.saveetha.in'
+]
 
 
 # Application definition
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'companywebsite'
 ]
 
 MIDDLEWARE = [
@@ -119,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
